930bb51829c54337e8c6e5f9b55b258bbee543cd
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-11-30 17:13:40 +0100

    Added the shared variables.

---
+++ shared/02_variables.zsh
@@ -0,0 +1,11 @@
+# Load local bin.
+export PATH="/usr/local/bin:$PATH"
+
+# Vim is awesome.
+export EDITOR=vim
+export VISUAL=mvim
+export GIT_EDITOR=vim
+export SVN_EDITOR=vim
+
+# Colors.
+export CLICOLOR=1
