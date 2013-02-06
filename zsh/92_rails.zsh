d1107d67922fc1cc499b73acb394c25ca5446159
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-02-06 14:04:41 +0100

    Move all output from the rails --version command to /dev/null.

--- zsh/92_rails.zsh
+++ zsh/92_rails.zsh
@@ -1,6 +1,6 @@
 # Retrieve the Rails version of the current directory.
 function rails_version {
-  current_version=$(rails --version | sed -E 's/Rails (.*)/v\1/g')
+  current_version=$(rails --version &> /dev/null | sed -E 's/Rails (.*)/ v\1/g')
   if [ $? ]; then
     echo "$gray$current_version$reset"
   fi
--- zsh/95_prompt.zsh
+++ zsh/95_prompt.zsh
@@ -1,4 +1,4 @@
 # Let's use an awesome prompt.
 setopt prompt_subst
 PROMPT='$(~/.osx-settings/prompt $?)'
-RPROMPT='$(___rvm_prompt) $(rails_version)'
+RPROMPT='$(___rvm_prompt)$(rails_version)'
