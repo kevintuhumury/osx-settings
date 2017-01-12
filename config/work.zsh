b281b22e09f7251b65075726e6982a4d9b6861c2
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2017-01-12 13:44:16 +0100

    Don’t create databases each time this is run. You will lose all data. We don’t want that.

--- config/work.zsh
+++ config/work.zsh
@@ -4,4 +4,4 @@ vlog() {
 
 # Platform related aliases
 alias ddy-clean='rake sync_git_repositories; vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; ./bin/run_bundle_install; ./bin/create_databases; start_apps" -- -t'
-alias vclean='vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; run_bundle_install; create_databases; seed_databases; start_apps" -- -t'
+alias vclean='vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; run_bundle_install; seed_databases; start_apps" -- -t'
