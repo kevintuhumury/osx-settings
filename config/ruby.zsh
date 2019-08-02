59f1dea8017dadfe53cf1abb82201ae09629a116
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2019-08-02 16:03:59 +0200

    Don’t use this at all.

--- config/ruby.zsh
+++ config/ruby.zsh
@@ -34,11 +34,3 @@ function b() {
     bundle exec $*
   fi
 }
-
-# Retrieve the Rails version of the current directory.
-function rails_version {
-  current_version=$(rails --version &> /dev/null | sed -E 's/Rails (.*)/ v\1/g')
-  if [ $? ]; then
-    echo "$gray$current_version$reset"
-  fi
-}
