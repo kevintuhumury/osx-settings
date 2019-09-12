eb6ab754349091c0a35504115159b364c23ac3c4
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2019-09-12 11:55:34 +0200

    DRY.

--- config/work.zsh
+++ config/work.zsh
@@ -1,2 +1,3 @@
-alias eid='vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; run_bundle_install; seed_databases; start_apps" -- -t'
-alias platform='rake sync_git_repositories; vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; ./bin/run_bundle_install; ./bin/create_databases; start_apps" -- -t'
+alias vagrant-clean='vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; run_bundle_install; create_databases; seed_databases; start_apps" -- -t'
+alias eid='vagrant-clean'
+alias platform='rake sync_git_repositories; vagrant-clean'
