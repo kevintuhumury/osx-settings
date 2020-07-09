62673a9e7823b31e62a826104ce04410863e634c
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2020-07-09 09:56:30 +0200

    Simplify.

--- config/work.zsh
+++ config/work.zsh
@@ -1,3 +1,3 @@
-alias vagrant-clean='vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; run_bundle_install; create_databases; seed_databases; start_apps" -- -t'
+alias vagrant-clean='vagrant up; vagrant ssh -c "cd /vagrant; start_services; start_apps; run_bundle_install; start_apps" -- -t'
 alias eid='vagrant-clean'
 alias platform='rake sync_git_repositories; vagrant-clean'
