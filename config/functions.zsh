c9f90287814fce84f2554cffc0f570ed27c104fa
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2020-07-09 09:57:56 +0200

    Easily setup a new origin for a git repository.

--- config/functions.zsh
+++ config/functions.zsh
@@ -1,3 +1,12 @@
+RESET="\033[0m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+
+info() {
+  echo -e "\n${YELLOW}${1}${RESET}"
+}
+
 # Delete the ._* files.
 function delete_dot_underscore {
   `find $1 -name "._*" -delete`
@@ -20,4 +29,24 @@ function digitalocean {
 # Easily set the git upstream.
 function git_upstream {
   git branch --set-upstream-to=origin/$1 $1
+}
+
+# Easily replace the git origin.
+function git_origin {
+  if [ ! -z "$1" ]; then
+    info 'Remote repositories (current):'
+    git remote -v
+
+    git remote rename origin temp
+    git remote add origin $1
+
+    info 'Remote repositories (temporary):'
+    git remote -v
+    git remote remove temp
+
+    info 'Remote repositories (new):'
+    git remote -v
+  else
+    info 'No argument supplied.'
+  fi
 }
